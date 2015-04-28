c0b6cc884234198bd02e5a1e95c5c65b28e0513b
edoher <hernandezmedia@gmail.com>
2015-04-28 16:49:35 +0200

    New vars and starting method

--- js.js
+++ js.js
@@ -2,15 +2,35 @@
 var score = 0;
 var totalRolls = 20;
 var extraRolls = 0;
-var rolls = new Array();
 var currentRoll = 1;
 var currentFrame = 1;
+var cannotPlay = false;
+
+var sumRolls = 0;
+
+var isStrike = false;
+var strikeXtra = 0;
+
+var isSpare = false;
+var spareXtra = 0;
 
 //Le functions!
-function roll() {
+function roll(){
+	var randy = Math.floor(Math.random() * 10) + 1;
+	
+	//add the random score to our sum
+	score += randy;
+	
+	currentRoll++;
+	if(currentRoll > 2){
+		currentFrame++;
+		currentRoll = 1;
+	}
+	
+	//all good? let's rock and roll!
     scoreTrack();
 }
 
-function scoreTrack() {
+function scoreTrack(){
     document.getElementById("track").innerHTML = score;
 }
