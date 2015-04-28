ae443117e096bc761800bcbb231336b6e32fe3eb
edoher <hernandezmedia@gmail.com>
2015-04-28 17:36:08 +0200

    New functions, new vars, reduced frames for testing

--- js.js
+++ js.js
@@ -1,6 +1,6 @@
 //Setup initial vars
 var score = 0;
-var totalRolls = 20;
+var totalFrames = 2;
 var extraRolls = 0;
 var currentRoll = 1;
 var currentFrame = 1;
@@ -16,24 +16,40 @@ var spareXtra = 0;
 
 //Le functions!
 function roll(){
-	var randy = Math.floor(Math.random() * 10) + 1;
-	
-	//add the random score to our sum
-	score += randy;
-	
-	currentRoll++;
-	if(currentRoll > 2){
-		currentFrame++;
-		currentRoll = 1;
+	if(cannotPlay == false){
+		var randy = Math.floor(Math.random() * 10) + 1;
+		
+		//add the random score to our sum
+		score += randy;
+		
+		currentRoll++;
+		if(currentRoll > totalFrames){
+			if(currentFrame < 2){
+				currentFrame++;
+				currentRoll = 1;
+			}else{
+				gameOver();
+			}
+		}
+		
+		//all good? let's rock and roll! show me the money!
+		showTrack();
+	}else{
+		alert("Game Over!");
 	}
-	
-	//all good? let's rock and roll!
-    scoreTrack();
 }
 
-function scoreTrack(){
+function showTrack(){
 	//basically print out the score and any extra info
     document.getElementById("frame").innerHTML = "Frame: " + currentFrame;
     document.getElementById("roll").innerHTML = "Roll: " + currentRoll;
     document.getElementById("track").innerHTML = score;
+}
+
+function gameOver(){
+	//no more fun!
+	cannotPlay = true;
+	document.getElementById("play").setAttribute('disabled','disabled');
+	currentRoll = '-';
+	currentFrame = '-';
 }
